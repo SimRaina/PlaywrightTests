4ad4fd382dc660bfd917539942ac56510f75ff09
Sim Raina <simran.pre90@gmail.com>
2026-05-28 12:51:36 +0200

    Refactor Playwright initialization in WaitsTest

--- src/test/java/uiTests/WaitsTest.java
+++ src/test/java/uiTests/WaitsTest.java
@@ -125,8 +125,7 @@ public class WaitsTest {
     }
 
     void openPage() {
-        playwright =
-                Playwright.create();
+        playwright = Playwright.create();
         browser = playwright.chromium().launch(
                 new BrowserType.LaunchOptions()
                         .setHeadless(false)
