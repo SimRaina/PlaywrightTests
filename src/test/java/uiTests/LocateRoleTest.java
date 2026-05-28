989db5c1e140c7e46f3d0778bf2854b484d07ee4
Sim Raina <simran.pre90@gmail.com>
2026-05-28 12:30:42 +0200

    Refactor Playwright initialization in LocateRoleTest

--- src/test/java/uiTests/LocateRoleTest.java
+++ src/test/java/uiTests/LocateRoleTest.java
@@ -57,8 +57,7 @@ public class LocateRoleTest {
      */
 
     void openPage() {
-        playwright =
-                Playwright.create();
+        playwright = Playwright.create();
         browser = playwright.chromium().launch(
                 new BrowserType.LaunchOptions()
                         .setHeadless(false)
