8d3ed4a5ce7f0b1ab44adabf42f56035c3fc8974
Sim Raina <simran.pre90@gmail.com>
2026-05-28 12:38:59 +0200

    Refactor Playwright initialization in test

--- src/test/java/uiTests/MockingAPITest.java
+++ src/test/java/uiTests/MockingAPITest.java
@@ -118,8 +118,7 @@ public class MockingAPITest {
     }
 
     void openPage() {
-        playwright =
-                Playwright.create();
+        playwright = Playwright.create();
         browser = playwright.chromium().launch(
                 new BrowserType.LaunchOptions()
                         .setHeadless(false)
